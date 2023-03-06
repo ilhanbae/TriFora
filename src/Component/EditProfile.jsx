66fc0dc7a2c4d0d21d32204f8772d11bf0f29d54
Jay <jaycheun@buffalo.edu>
2023-03-06 18:12:47 -0500

    Implemented React and CSS code for Edit Profile #64

---
+++ src/Component/EditProfile.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./EditProfile.css";
+import harold from "../assets/harold.jpg";
+import yellowbox from "../assets/yellowbox.jpg";
+import blackbox from "../assets/blackbox.png";
+
+class EditProfile extends React.Component {
+    render(){
+        return(
+            
+            <form>
+
+                <label classname = "textbox">
+                   <input type="text" placeholder="Username" className="user"/>
+                </label>
+
+                <label classname = "textbox">
+                   <input type="text" placeholder="First" className="first" />
+                </label>
+
+                <label classname = "textbox">
+                   <input type="text" placeholder="Last" className="last" />
+                </label>
+
+                <label classname = "textbox">
+                   <input type="text" placeholder="Bio" className="bio" />
+                </label>
+
+                <label className="upload">
+                    Upload
+                    <input type='file' id='file-upload' className='empty'/>
+                </label>
+
+                <button className="save">Save</button>
+
+                <button className="cancel">Cancel</button>
+
+                <button className="remove">Remove Account</button>
+                
+                <input className = 'pfp' type='image' src={harold} alt='filler'/>
+
+                <div className="choose">
+                    <h1>Choose Displayed Communities</h1>
+                </div>
+
+                <input className = 'choosebox' type='image' src={yellowbox} alt='filler'/>
+
+                <input className = 'b1' type='image' src={blackbox} alt='filler'/>
+
+                <input className = 'b2' type='image' src={blackbox} alt='filler'/>
+
+                <input className = 'b3' type='image' src={blackbox} alt='filler'/>
+
+                <input className = 'b4' type='image' src={blackbox} alt='filler'/>
+
+                <div className="c1">
+                    <h1>Class of 2023</h1>
+                </div>
+
+                <div className="c2">
+                    <h1>DCEU</h1>
+                </div>
+
+                <div className="c3">
+                    <h1>Marvel CU</h1>
+                </div>
+
+                <div className="c4">
+                    <h1>CW</h1>
+                </div>
+
+                <div className="checkbox1"> 
+                    <input type="checkbox" /> 
+                </div>
+
+                <div className="checkbox2"> 
+                    <input type="checkbox" /> 
+                </div>
+
+                <div className="checkbox3"> 
+                    <input type="checkbox" /> 
+                </div>
+
+                <div className="checkbox4"> 
+                    <input type="checkbox" /> 
+                </div>
+ 
+            </form>
+            
+        )
+    }
+}
+
+export default EditProfile;
