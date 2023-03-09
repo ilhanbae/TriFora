7386bd64e034dca54f7cc189f5edabd691c86d70
BensonCai <bensonca@buffalo.edu>
2023-03-09 16:43:54 -0500

    Forgot Password Page #69

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ForgotPass from "./Component/ForgotPass";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ForgotPass />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
