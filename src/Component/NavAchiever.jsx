fafb9bbcbbb17a6b4187e5da8121fd5f1b258da9
Joe Conkling <Jconkling619@yahoo.com>
2023-03-18 02:31:22 -0400

    styling should be finished #62

--- src/Component/NavAchiever.jsx
+++ src/Component/NavAchiever.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./NavAchiever.css";
 import { Link } from "react-router-dom";
 import groupIcon from "../assets/group.png";
+import downIcon from "../assets/downIcon36.png";
+import upIcon from "../assets/upIcon36.png";
 // import DropMenu from "./DropMenu";
 
 class NavAchiever extends React.Component {
@@ -16,7 +18,7 @@ class NavAchiever extends React.Component {
 
 
     menuSwitch = () => {
-        this.setState(prevState => ({ showDropMenu: !prevState.showDropMenu }));
+        this.setState(menuState => ({ showDropMenu: !menuState.showDropMenu }));
     };
 
     render() {
@@ -25,31 +27,38 @@ class NavAchiever extends React.Component {
                 {/* items that appear to the left side of the navbar */}
                 <ul id="leftItems">
                     <li className="logo">
-                        <Link to="/"> Logo </Link>
+                        {/* <Link to="/"> Logo </Link> */}
+                        {/* Temporarily non clickable text since destination unknown */}
+                        <div> Logo </div>
                     </li>
+
+                    {/* commented out as lacking functionality for now */}
                     {/* About/browse link will be changing depending on page, see search bar comment in next div */}
-                        {/* this.props.navStyle === 2 ? (about page link will go here):(browse link will go here) */}
-                       { /* browse may end up being a logged in only function? this comment was a part of the above browse link */}
+                    {/* this.props.navStyle === 2 ? (about page link will go here):(browse link will go here) */}
+                    { /* browse may end up being a logged in only function? this comment was a part of the above browse link */}
 
                 </ul>
+                
+                {/* commented out as lacking functionality for now */}
                 {/* {sessionStorage.getItem("token") && */}
-                    {/* The middle item search bar only occurs while logged in and only on certain pages. There will exist a state variable
+                {/* The middle item search bar only occurs while logged in and only on certain pages. There will exist a state variable
                     in the main app that needs to potentially be updated whenever a page loads */}
-                    {/* <div>
+                {/* <div>
                         {this.props.navStyle === 3 &&
                             <input type="text" placeholder="search function" />}
                     </div>
                 } */}
+
                 {/* Items on the right side of the navbar are handled here, drop down included */}
                 <ul id="rightItems">
                     {!sessionStorage.getItem("token") &&
                         /*  Making use of the way the boilerplate code handled login seems to work
                             if logged out show signup/login */
                         <>
-                            <li id="sign up">
-                                <Link to="/signup"> Sign up </Link>
+                            <li className="nav_text">
+                                <Link to="/register"> Sign up </Link>
                             </li>
-                            <li id="login">
+                            <li className="nav_text">
                                 <Link to="/login"> Login </Link>
                             </li>
                         </>}
@@ -57,16 +66,31 @@ class NavAchiever extends React.Component {
                         /* if logged in have a dropdown menu with the profile icon*/
                         // Icon/button needs to be changed, just using as a placeholder for testing
                         <li className="pm admin">
-                            <button
-                                className="profile-holder"
-                                onClick={this.menuSwitch}
-                            >
+                            {/* This icon is a placeholder until maybe profile icon */}
+                            <img
+                                src={groupIcon}
+                                className="profile-icon"
+                                // onClick={this.menuSwitch}
+                                alt="profile icon here"
+                                title="profile icon here" />
+                            {/* Icon change based on menu state (starts false as menu is hidden) */}
+                            {this.state.showDropMenu ? (
                                 <img
-                                    src={groupIcon}
-                                    className="profile-icon"
-                                    alt="profile"
-                                    title="profile" />
-                            </button>
+                                    src={upIcon}
+                                    className="up-icon"
+                                    onClick={this.menuSwitch}
+                                    alt="hide menu"
+                                    title="hide menu"
+                                    style={{cursor:"pointer"}} />
+                            ) : (
+                                <img
+                                    src={downIcon}
+                                    className="down-icon"
+                                    onClick={this.menuSwitch}
+                                    alt="show menu"
+                                    title="show menu"
+                                    style={{cursor:"pointer"}} />
+                            )}
                             {/* <DropMenu /> */}
                             {/* DropMenu could now likely be switch to being it's own component, though a way to logout would
                                     have to be passed through */}
@@ -79,13 +103,20 @@ class NavAchiever extends React.Component {
                                     <Link to="/notifications" onClick={this.menuSwitch}> Notifications </Link>
                                 </li> */}
                                 <li>
-                                    <Link to="/" onClick={e => {
-                                        /* this is the only way I could think to do this for logout */
+                                    <Link to="/login" onClick={e => {
+                                        {/* this is the only way I could think to do this for logout */}
                                         this.props.logout(e);
                                         this.menuSwitch();
                                     }}>
                                         Logout
                                     </Link>
+                                    {/* <div style={{cursor:"pointer"}} onClick={e => { */}
+                                        {/* this is the only way I could think to do this for logout */}
+                                        {/* this.props.logout(e);
+                                        this.menuSwitch();
+                                    }}>
+                                        Logout
+                                    </div> */}
                                 </li>
                             </ul>
 
