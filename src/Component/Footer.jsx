e3abddc64ea747dfade7c36cfdb439df9b579acc
Joe Conkling <Jconkling619@yahoo.com>
2023-05-02 23:19:41 -0400

    trying another fix for style guide link bug #112

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <Link to="about" className="footer-links">About us</Link>
-            <a href="../style-guide.html" className="footer-links">Style guide</a>
+            <a href="./style-guide.html" className="footer-links">Style guide</a>
         </div>
     )
 }
