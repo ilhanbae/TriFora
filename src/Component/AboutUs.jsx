773a399717b9d29971d9185e3f893dd1e293623b
Joe Conkling <Jconkling619@yahoo.com>
2023-05-03 23:30:10 -0400

    about description update #113

--- src/Component/AboutUs.jsx
+++ src/Component/AboutUs.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import "../style/AboutUs.css";
 import BackButton from "./BackButton.jsx";
 
-// making this js instead of jsx for public html use, not sure if any problems will arise
 export default function AboutUs() {
     return (
         <div className="about-page-wrapper"> {/* wrapper */}
@@ -13,11 +12,13 @@ export default function AboutUs() {
             <div className="about-description-box">
                 <div className="about-team-title">
                     <h1>
-                    Trifora
+                    Why Trifora?
                     </h1>
                 </div>
                 <div className="about-team-description">
-                Our goal is to have Trifora be a place where users can find, connect, and build a community of their interest. Why Trifora? because we believe our communities can unite people given a place like a open Forum.
+                Our hope is to have Trifora be a place where users can find, connect, and build a community of their interest. 
+                We believe our communities can unite people just like they were part of an open Forum. Trifora's focus is to have our users
+                be univeristy audiences, and hope to connect students with other like-minded students to share information and thoughts.
                 </div>
             </div>
             <div className="about-links-box">
