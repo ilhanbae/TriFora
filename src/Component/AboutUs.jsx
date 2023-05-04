edfb13806fc0586504c739d49ee04938e1653689
Joe Conkling <Jconkling619@yahoo.com>
2023-05-03 21:17:52 -0400

    back button and about us stuff #113

--- src/Component/AboutUs.jsx
+++ src/Component/AboutUs.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "../style/AboutUs.css";
+import BackButton from "./BackButton.jsx";
 
+// making this js instead of jsx for public html use, not sure if any problems will arise
 export default function AboutUs() {
     return (
         <div className="about-page-wrapper"> {/* wrapper */}
+            <div className="about-back-button">
+                <BackButton />
+            </div>
             <div className="about-description-box">
                 <div className="about-team-title">
                     <h1>
-                        Underachievers
+                    Trifora
                     </h1>
                 </div>
                 <div className="about-team-description">
-                    description test
+                Our goal is to have Trifora be a place where users can find, connect, and build a community of their interest. Why Trifora? because we believe our communities can unite people given a place like a open Forum.
                 </div>
             </div>
             <div className="about-links-box">
